45c483e5d795acaf9fe83e9c81a28726ec318b14
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2023-02-20 07:35:39 +1100

    Created basic component

---
+++ Scrimba-LearnReactForFreeCourse/React-AirBnb-Clone/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "./assets/components/Navbar";
+
+export default function App() {
+  return(
+    <Navbar/>
+  )
+}
---
+++ Scrimba-LearnReactForFreeCourse/React-AirBnb-Clone/src/assets/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return(
+    <div className="Navbar">
+      <h1>TEST</h1>
+      <img src="" alt="Airbnb-Logo" />
+    </div>
+  )
+}
