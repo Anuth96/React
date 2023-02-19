92f90aa52446ff3a46bceccba82332ea1686d773
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2023-02-20 10:56:07 +1100

    Component for Hero Section created and styling completed

---
+++ Scrimba-LearnReactForFreeCourse/React-AirBnb-Clone/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Hero() {
+  return(
+    <section className="hero-component">
+      <img src="src/assets/grid-image.png" alt="airbnb-grid-images" className="images-grid"/>
+      <h1 className="hero-h1">Online Experiences</h1>
+      <p className="hero-p">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+    </section>
+  )
+}
