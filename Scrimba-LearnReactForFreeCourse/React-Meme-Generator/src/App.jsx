52374ce42450971f0a0d84736f510add5c21c55d
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2023-02-25 09:52:06 +1100

    Created Skeletal Form component and redered in app.jsx

--- Scrimba-LearnReactForFreeCourse/React-Meme-Generator/src/App.jsx
+++ Scrimba-LearnReactForFreeCourse/React-Meme-Generator/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Form from "./components/Forms";
 
 export default function App() {
   return(
-    <Header />
+    <div>
+      <Header />
+      <Form />
+    </div>
   )
 }
