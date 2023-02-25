d73223ac368b08cb9b3652bdaa8cf3722fae2947
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2023-02-25 15:00:16 +1100

    Added memesdata file

--- Scrimba-LearnReactForFreeCourse/React-Meme-Generator/src/components/Forms.jsx
+++ Scrimba-LearnReactForFreeCourse/React-Meme-Generator/src/components/Forms.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function Form() {
+  function handleClick() {
+    console.log('I was clicked');
+  }
+
   return(
     <main>
-      <form className="form">
+      <div className="form">
           <input type="text" placeholder="Top text" className="form--input"/>
           <input type="text" placeholder="Bottom text" className="form--input"/>
-          <button className="form--button"> Get a new meme image 🖼 </button>
-      </form>
+          <button onClick={handleClick} className="form--button"> Get a new meme image 🖼 </button>
+      </div>
     </main>
   )
 }
